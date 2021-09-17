963fb7b628dac76cbc06dac8615f6536d5997ff7
HanYong <50603217+toy9910@users.noreply.github.com>
2021-09-17 14:56:00 +0900

    Update 1773.cpp

--- 백준/1773.cpp
+++ 백준/1773.cpp
@@ -2,8 +2,8 @@
 
 using namespace std;
 
-bool arr[2000001];
-vector<int> times;
+bool arr[2000001];	// 해당 시간에 폭죽이 터지는지 확인하는 배열
+vector<int> times;	// 폭죽이 터지는 시간들을 저장하는 벡터
 
 int main() {
 	ios::sync_with_stdio(0);
@@ -16,7 +16,7 @@ int main() {
 		int num;
 		cin >> num;
 		for (int j = num; j <= C; j += num) {
-			if (!arr[j]) {
+			if (!arr[j]) {	// 해당 시간에 폭죽이 터지지 않는다면
 				arr[j] = true;
 				times.push_back(j);
 			}
