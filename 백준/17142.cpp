1079a2770e8496f5b34d43d6b6f9baa9362069b3
HanYong <toy9910@naver.com>
2021-11-29 21:12:25 +0900

    연구소 3

--- 백준/17142.cpp
+++ 백준/17142.cpp
@@ -6,27 +6,26 @@ int dx[] = { -1,0,1,0 };
 int dy[] = { 0,-1,0,1 };
 
 vector<vector<int>> board(51, vector<int>(51));
-vector<pair<int, int>> virus;
-vector<pair<int, int>> tmp(11);
-vector<int> answers;
-int N, M,answer, zcount;
+vector<pair<int, int>> virus; // 모든 바이러스의 위치
+vector<pair<int, int>> tmp(11); // 조합으로 생성한 바이러스들
+vector<int> answers; // 각 경우의 걸린 시간들 저장
+int N, M, answer, zcount;
 
 void func(int depth, int next) {
 	if (depth == M) {
 		answer = 0;
-		vector<vector<int>> dis(51, vector<int>(51, 0));
-		vector<vector<bool>> visited(51, vector<bool>(51, false));
+		vector<vector<int>> dis(51, vector<int>(51, 0)); // 걸린 시간
+		vector<vector<bool>> visited(51, vector<bool>(51, false)); // 방문 표시
 		queue<pair<int, int>> q;
 
-
+		// 큐에 M개의 활성화 바이러스 넣기
 		for (int i = 0; i < M; i++) {
 			q.push({ tmp[i] });
 			visited[tmp[i].first][tmp[i].second] = true;
 		}
 
-		int ccount = 0;
-		
-		
+		int ccount = 0; // 빈 칸 -> 바이러스가 된 갯수
+
 		while (!q.empty()) {
 			auto cur = q.front();
 			q.pop();
@@ -41,7 +40,9 @@ void func(int depth, int next) {
 					continue;
 				if (board[xpos][ypos] == 0) // 빈 칸 -> 바이러스 체크
 					ccount++;
-				dis[xpos][ypos] = dis[cur.first][cur.second] + 1;
+				dis[xpos][ypos] = dis[cur.first][cur.second] + 1; // 걸린 시간 업데이트
+
+				// 모든 빈 칸이 바이러스가 된 경우
 				if (zcount == ccount) {
 					answer = max(dis[xpos][ypos], answer);
 					answers.push_back(answer);
@@ -51,25 +52,10 @@ void func(int depth, int next) {
 				visited[xpos][ypos] = true;
 				q.push({ xpos,ypos });
 			}
-		} 
-		
-		/*
-		* cout << '\n';
-		 for (int i = 0; i < N; i++) {
-			for (int j = 0; j < N; j++) {
-				cout << dis[i][j] << ' ';
-			}
-			cout << '\n';
 		}
-		 cout << '\n';
-		*/
-		
-		
-		
-		
-			
 	}
 	else {
+		// 재귀로 조합 생성
 		for (int i = next; i < virus.size(); i++) {
 			tmp[depth] = virus[i];
 			func(depth + 1, i + 1);
@@ -84,6 +70,7 @@ int main() {
 
 	cin >> N >> M;
 
+	// 빈 칸 갯수
 	zcount = 0;
 
 	for (int i = 0; i < N; i++) {
@@ -96,21 +83,21 @@ int main() {
 		}
 	}
 
+	// 빈 칸이 없는 경우
 	if (zcount == 0) {
 		cout << 0;
 		return 0;
 	}
 
 	func(0, 0);
+	// 모든 빈 칸에 바이러스를 퍼뜨릴 수 있는 경우
 	if (!answers.empty()) {
+		// 정렬 후 최솟값 출력
 		sort(answers.begin(), answers.end());
 		cout << answers[0];
 	}
-	else
+	else // 모든 빈 칸에 바이러스를 퍼뜨릴 수 없는 경우
 		cout << -1;
-
-	//for (auto num : answers)
-		//cout << num << '\n';
 
 	return 0;
 }
